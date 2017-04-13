5acf069de84bf6cee7abc8078176c4ac42eb25ae
ethanryan <ethanryan@gmail.com>
2017-04-13 13:29:30 -0400

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true, uniqueness: true
+  #validates :email, presence: true, uniqueness: true
+  validates :content, length: { minimum: 250 }
+  #validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  #validate :clickbait_title, on: :create
 end
